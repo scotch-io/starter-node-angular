43d594bd92560cf3ec0f965e4ce5b57ddc28b56d
Caio Mariano <caio.mariano@unicesumar.edu.br>
2014-07-01 15:05:57 -0300

    upgrading express3 to 4,mongoose,bodyParser and method-override

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var app     = express();
 var mongoose= require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 // configuration ===========================================
 	
@@ -11,12 +13,12 @@ var db = require('./config/db');
 var port = process.env.PORT || 8080; // set our port
 // mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
 
-app.configure(function() {
-	app.use(express.static(__dirname + '/public')); 	// set the static files location /public/img will be /img for users
-	app.use(express.logger('dev')); 					// log every request to the console
-	app.use(express.bodyParser()); 						// pull information from html in POST
-	app.use(express.methodOverride()); 					// simulate DELETE and PUT
-});
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(express.static(__dirname + '/public')); 	// set the static files location /public/img will be /img for users
 
 // routes ==================================================
 require('./app/routes')(app); // pass our application into our routes
