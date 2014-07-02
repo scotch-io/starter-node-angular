4cfe102952cfaf5626564d15c84b2b22a4601c3a
Caio Mariano <caio.mariano@unicesumar.edu.br>
2014-07-02 13:46:06 -0300

    putting comments on all new lines.

--- server.js
+++ server.js
@@ -13,12 +13,13 @@ var db = require('./config/db');
 var port = process.env.PORT || 8080; // set our port
 // mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
 
-app.use(bodyParser.json());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-app.use(bodyParser.urlencoded({extended: true}));
+//get all data/stuff of the body (POST) parameters
+app.use(bodyParser.json()); // parse application/json 
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(bodyParser.urlencoded({extended: true})); // parse application/x-www-form-urlencoded
 
-app.use(methodOverride('X-HTTP-Method-Override'));
-app.use(express.static(__dirname + '/public')); 	// set the static files location /public/img will be /img for users
+app.use(methodOverride('X-HTTP-Method-Override')); // simulate delete/put
+app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
 
 // routes ==================================================
 require('./app/routes')(app); // pass our application into our routes
