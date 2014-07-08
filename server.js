7bce5126a43ce77eec4ed16f53126fb8a07e37e3
Chris Sevilleja <chris@scotch.io>
2014-07-08 09:07:08 -0700

    adding comments

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // modules =================================================
-var express = require('express');
-var app     = express();
-var mongoose= require('mongoose');
-var bodyParser = require('body-parser');
+var express        = require('express');
+var app            = express();
+var mongoose       = require('mongoose');
+var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 
 // configuration ===========================================
@@ -16,9 +16,9 @@ var port = process.env.PORT || 8080; // set our port
 //get all data/stuff of the body (POST) parameters
 app.use(bodyParser.json()); // parse application/json 
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(bodyParser.urlencoded({extended: true})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 
-app.use(methodOverride('X-HTTP-Method-Override')); // simulate delete/put
+app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
 
 // routes ==================================================
