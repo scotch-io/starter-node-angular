f856e31ede797c9fcf1203839f8a96a5b5340eaa
Chris Sevilleja <chris@scotch.io>
2014-10-12 09:12:10 -0700

    Update Nerd.js

---
+++ app/models/Nerd.js
@@ -0,0 +1,8 @@
+// grab the mongoose module
+var mongoose = require('mongoose');
+
+// define our nerd model
+// module.exports allows us to pass this to other files when it is called
+module.exports = mongoose.model('Nerd', {
+	name : {type : String, default: ''}
+});
