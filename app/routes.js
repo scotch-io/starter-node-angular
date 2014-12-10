cd1bb2b2cf952aefd68e133a535dae2d6bc21d2d
Zemke <florian.zemke@ewetel.net>
2014-12-10 13:58:56 +0100

    Now there's Jade.

--- app/routes.js
+++ app/routes.js
@@ -1,13 +1,16 @@
 module.exports = function(app) {
 
-	// server routes ===========================================================
-	// handle things like api calls
-	// authentication routes
-
-	// frontend routes =========================================================
-	// route to handle all angular requests
-	app.get('*', function(req, res) {
-		res.sendfile('./public/index.html');
+	// Anything but the AngularJS routes (/view) should render index.
+	app.all('/*', function(req, res, next) {
+		if (req.url.split('/')[1] !== 'views') {
+			res.render('index');
+		} else {
+			next();
+		}
 	});
 
+	// Front-end routes to handle all AngularJS requests.
+	app.get('/views/:partial', function(req, res, next) {
+		res.render('views/' + req.params.partial);
+	});
 };
