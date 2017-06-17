162dcd19161001cd26680d69186bbe723f695f05
Lindsey <ldunphy@bu.edu>
2017-06-17 13:03:38 -0400

    able to iterate through recipes

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,19 @@
 let express = require('express')
 const router = express.Router()
 const FFKEY = require('../config/food2forkAPI')
+let f2fSearchResponse =
+    {
+        "count": 1,
+        "recipes": [{
+            "publisher": "Allrecipes.com",
+            "social_rank": 99.81007979198002,
+            "f2f_url": "http://food2fork.com/F2F/recipes/view/29159",
+            "publisher_url": "http://allrecipes.com",
+            "title": "Slow-Cooker Chicken Tortilla Soup",
+            "source_url": "http://allrecipes.com/Recipe/Slow-Cooker-Chicken-Tortilla-Soup/Detail.aspx",
+            "page": 1
+        }]
+    }
 
 router.get('/getRecipes', function (req, res, next) {
     let request = require("request");
@@ -12,6 +25,11 @@ router.get('/getRecipes', function (req, res, next) {
 
     request(options, function (error, response, body) {
         if (error) throw new Error(error);
+        f2fSearchResponse=JSON.parse(body)
+        //console.log(f2fSearchResponse.recipes)
+        f2fSearchResponse.recipes.forEach( function (recipe) {
+            console.log(recipe.title)
+        })
         res.json(body)
     });
 })
