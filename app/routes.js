0937ae1f2d2a83be4715cc5e7201924723888675
Lindsey <ldunphy@bu.edu>
2017-06-19 10:18:12 -0400

    F2F api working!

--- app/routes.js
+++ app/routes.js
@@ -3,17 +3,10 @@ const router = express.Router()
 const FFKEY = require('../config/food2forkAPI')
 let f2fSearchResponse =
     {
-        "count": 1,
-        "recipes": [{
-            "publisher": "Allrecipes.com",
-            "social_rank": 99.81007979198002,
-            "f2f_url": "http://food2fork.com/F2F/recipes/view/29159",
-            "publisher_url": "http://allrecipes.com",
-            "title": "Slow-Cooker Chicken Tortilla Soup",
-            "source_url": "http://allrecipes.com/Recipe/Slow-Cooker-Chicken-Tortilla-Soup/Detail.aspx",
-            "page": 1
-        }]
+        "count": undefined,
+        "recipes": []
     }
+let f2fGetRecipeResponse = []
 
 router.get('/getRecipes', function (req, res, next) {
     let request = require("request");
@@ -25,12 +18,25 @@ router.get('/getRecipes', function (req, res, next) {
 
     request(options, function (error, response, body) {
         if (error) throw new Error(error);
-        f2fSearchResponse=JSON.parse(body)
-        //console.log(f2fSearchResponse.recipes)
-        f2fSearchResponse.recipes.forEach( function (recipe) {
-            console.log(recipe.title)
+        f2fSearchResponse = JSON.parse(body)
+        f2fSearchResponse.recipes.forEach(function (recipe) {
+            console.log(recipe.recipe_id)
         })
-        res.json(body)
+        res.json(f2fSearchResponse)
+    });
+})
+router.get('/:recipeId', function (req, res, next) {
+    let request = require("request");
+    let options = {
+        method: 'POST',
+        url: 'http://food2fork.com/api/get',
+        form: {key: FFKEY.key, rId: req.params.recipeId}
+    };
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        f2fGetRecipeResponse = JSON.parse(body)
+        res.json(f2fGetRecipeResponse)
     });
 })
 
